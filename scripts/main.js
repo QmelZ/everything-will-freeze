9ce9aba5ece26e5aa2377d6d6e15c8114aa92510
QmelZ <qmelzthevoid@gmail.com>
2021-02-05 12:53:18 +0300

    time freeze

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+let zero = new Packages.java.lang.Integer(0);
+Core.settings.put("musicvol", zero);
+Core.settings.put("sfxvol", zero);
+Core.settings.put("ambientvol", zero);
+
+let timeFreeze = loadMusic("everything-will-freeze");
+Events.on(ClientLoadEvent, () => {
+    timeFreeze.setLooping(true);
+    timeFreeze.play();
+    timeFreeze.setVolume(1);
+});
+
+Events.on(WorldLoadEvent, () => {
+    Timer.schedule(() => {
+        Events.run(Trigger.update, () => {
+            Time.setDeltaProvider(
+                () => {
+                    return 0;
+                }
+            );
+            Vars.control.input.block = null;
+        });
+    }, Mathf.random(60) + 30);
+});
